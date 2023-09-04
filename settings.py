4057b21f4134679e5b0c368bd2164504de2cbbc8
maze <invoker322sf@gmail.com>
2023-09-05 00:49:00 +1000

    refactoring to one docker-compose and .env files

--- settings.py
+++ settings.py
@@ -1,28 +1,31 @@
 from dotenv import load_dotenv
 from envparse import env
 
+
+load_dotenv('.env')
 DEV = env.bool("DEV")
+DB_USER_TEST = env.str('POSTGRES_USER_TEST')
+DB_PASSWORD_TEST = env.str('POSTGRES_PASSWORD_TEST')
+DB_HOST_TEST = env.str('DB_HOST_TEST')
+DB_PORT_TEST = env.int('DB_PORT_TEST')
+DB_NAME_TEST = env.str('POSTGRES_DB_TEST')
 
-load_dotenv('.env.test')
-DB_USER_TEST = env.str('POSTGRES_USER')
-DB_PASSWORD_TEST = env.str('POSTGRES_PASSWORD')
-DB_HOST_TEST = env.str('DB_HOST')
-DB_PORT_TEST = env.int('DB_PORT')
-DB_NAME_TEST = env.str('POSTGRES_DB')
+print(DEV)
 
-load_dotenv('.env') if not DEV else load_dotenv('.env.dev')
+if not DEV:
+    DB_USER = env.str('POSTGRES_USER')
+    DB_PASSWORD = env.str('POSTGRES_PASSWORD')
+    DB_HOST = env.str('DB_HOST')
+    DB_PORT = env.int('DB_PORT')
+    DB_NAME = env.str('POSTGRES_DB')
+else:
+    DB_USER = env.str('POSTGRES_USER_DEV')
+    DB_PASSWORD = env.str('POSTGRES_PASSWORD_DEV')
+    DB_HOST = env.str('DB_HOST_DEV')
+    DB_PORT = env.int('DB_PORT_DEV')
+    DB_NAME = env.str('POSTGRES_DB_DEV')
 
-DB_USER = env.str('POSTGRES_USER')
-DB_PASSWORD = env.str('POSTGRES_PASSWORD')
-DB_HOST = env.str('DB_HOST')
-DB_PORT = env.int('DB_PORT')
-DB_NAME = env.str('POSTGRES_DB')
-
-
-DATABASE_URL = env.str(
-    "REAL_DATABASE_URL",
-    default=f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
 
 ASYNC_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(ASYNC_DATABASE_URL)
 DATABASE_TEST_URL = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASSWORD_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
